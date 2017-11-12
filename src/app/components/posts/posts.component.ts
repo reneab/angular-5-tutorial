b4d6441b1da3dfa1982c094d1a5e0c459ebd95f1
Rene Abdallah <rene@localhost.localdomain>
2017-11-13 00:05:17 +0800

    add posts component

---
+++ src/app/components/posts/posts.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class PostsComponent implements OnInit {
+
+  posts: Post[];
+
+  constructor(private data: DataService) { 
+    this.posts = [];
+  }
+
+  ngOnInit() {
+    this.data.getPosts().subscribe(p => p.forEach(el => {
+      console.log('Got post: ' + el.title);
+      this.posts.push(el);
+    }));
+  }
+
+}
+
+interface Post {
+  userId: number,
+  id: number,
+  title: string,
+  body: string
+}
